29bdad7d88b62537b3243eebeff8ad011d0d470e
Udit Sharma <sharma2004udit@gmail.com>
2026-02-23 19:01:28 +0530

    Configure Mongodb connection and added custom listing data

---
+++ init/index.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../Models/listing.js");
+
+// Establish a Connection
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+main()
+  .then (() => {
+     console. log("connected to Database");
+   })
+  .catch((err) => {
+     console. log(err);
+   });
+
+
+async function main() {
+    await mongoose.connect(MONGO_URL);
+}
+
+//Intialize the database
+const initDB = async () => {
+    await Listing.deleteMany({});
+    await Listing.insertMany(initData.data);
+    console.log("Data was initialized");
+}
+
+initDB();
