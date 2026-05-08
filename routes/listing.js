7d19bff4125915ffc0567da63987b3e5bb0eda54
Udit Sharma <sharma2004udit@gmail.com>
2026-05-09 00:19:09 +0530

    feat: implement post-login redirection and modularized access control middleware

--- routes/listing.js
+++ routes/listing.js
@@ -4,6 +4,7 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const ExpressError = require("../utils/ExpressError.js");
 const { listingSchema, reviewSchema } = require("../schema.js");
 const Listing = require("../models/listing.js");
+const {isLoggedIn} = require("../middleware.js");
 
 const validateListing = (req, res, next) => {
     let { error } = listingSchema.validate(req.body);
@@ -24,12 +25,15 @@ router.get("/",
 }));
 
 // new route
-router.get("/new", async (req, res) => {
+router.get("/new", 
+    isLoggedIn,
+    (req, res) => {
     res.render("listings/new.ejs");
 })
 
 // Create route - To perform create operations
 router.post("/",
+    isLoggedIn,
     validateListing,
     wrapAsync(async (req, res, next) => {
     const newListing = new Listing(req.body.listing);
@@ -52,6 +56,7 @@ router.get("/:id",
 
 // edit route
 router.get("/:id/edit",
+    isLoggedIn,
     wrapAsync (async (req, res) => {
     let {id} = req.params;
     const listing = await Listing.findById(id);
@@ -64,6 +69,7 @@ router.get("/:id/edit",
 
 // update route
 router.put("/:id",
+    isLoggedIn,
     validateListing, 
     wrapAsync (async (req, res) => {
     let {id} = req.params;
@@ -74,6 +80,7 @@ router.put("/:id",
 
 // delete route
 router.delete("/:id",
+    isLoggedIn,
     wrapAsync (async (req, res) => {
     let {id} = req.params;
     const deletedListing = await Listing.findByIdAndDelete(id);
--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 const wrapAsync = require("../utils/wrapAsync");
-const passport = require("passport")
+const passport = require("passport");
+const { saveRedirectUrl } = require("../middleware");
 
 router.get("/signup", (req, res) =>{
     res.render("../views/users/signup.ejs");
@@ -14,9 +15,13 @@ router.post("/signup",
         let {username, email, password} = req.body;
         const newUser = new User({email, username});
         const registeredUser = await User.register(newUser, password);
-        console. log(registeredUser);
-        req. flash("success", "Welcome to Wanderlust!");
-        res. redirect("/listings");
+        req.login(registeredUser, (err) => {
+            if (err) {
+                return next(err);
+            }
+            req. flash("success", "Welcome to Wanderlust!");
+            res. redirect("/listings");
+       });
     }catch(e){
         req.flash("error", e.message);
         res.redirect("/signup");
@@ -25,17 +30,29 @@ router.post("/signup",
 }));
 
 router.get("/login", (req, res) =>{
-    res. render("users/login.ejs");
+    res.render("users/login.ejs");
 });
 
 router.post('/login',
-passport.authenticate('local', { 
-    failureRedirect: '/login', 
-    failureFlash : true
-}),
-async (req, res)=> {
-    req.flash("success","Welcome to Wanderlust! You are logged in!");
-    res.redirect("/listings");
+    saveRedirectUrl,
+    passport.authenticate('local', { 
+        failureRedirect: '/login', 
+        failureFlash : true
+    }),
+    async (req, res)=> {
+        req.flash("success","Welcome to Wanderlust! You are logged in!");
+        let redirectUrl = res.locals.redirectUrl || "/listings";
+        res.redirect(redirectUrl);
+});
+
+router.get("/logout", (req, res) => {
+    req.logout((err) => {
+    if (err){
+        return next(err);
+    }
+        req.flash("success", "logged you out!");
+        res.redirect("/listings");
+    });
 });
 
 module.exports = router;
