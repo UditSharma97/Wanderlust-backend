91011525330b405895bb510c8c6db635654e50ce
Udit Sharma <sharma2004udit@gmail.com>
2026-06-27 23:37:25 +0530

    feat: implement UI for search bar, category filters, and tax toggle switch

--- app.js
+++ app.js
@@ -59,9 +59,9 @@ const sessionOptions = {
     },
 };
 
-app.get("/",(req, res) => {
-    res.send("HI, i am root.")
-})
+// app.get("/",(req, res) => {
+//     res.send("HI, i am root.")
+// })
 
 app.use(session(sessionOptions));
 app.use(flash());
